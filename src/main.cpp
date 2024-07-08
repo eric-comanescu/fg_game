ca01db06d31212e2c4bc9fa4626d42cb581a0f06
Eric Comanescu <117883271+BreadBurgIar@users.noreply.github.com>
2024-07-08 14:16:57 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <raylib.h>
+
+constexpr int W_WIDTH = 640;
+constexpr int W_HEIGHT = 360;
+constexpr int W_SCALE = 2;
+
+int main() {
+    InitWindow(W_WIDTH * W_SCALE, W_HEIGHT * W_SCALE, "Gaem");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Test", 0, 0, 24, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
