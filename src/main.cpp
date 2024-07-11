af43105eb8189f30c57b146e1e47333002a97daa
Ferret <ericcom@outlook.com>
2024-07-11 14:17:39 -0400

    added notes for windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include "../include/raylib.h"
 
+#include <cstdlib>
+
 constexpr int W_WIDTH = 640;
 constexpr int W_HEIGHT = 360;
 constexpr int W_SCALE = 2;
 
 int main() {
     InitWindow(W_WIDTH * W_SCALE, W_HEIGHT * W_SCALE, "Gaem");
+    SetExitKey(0);
     SetTargetFPS(60);
+
+    // TODO: if def __WIN32 (or __CYGWIN__ ?) cd to $home_dir\appdata\local and make folder
+    // else cd to .local and make folder there
+    // also include <filesystem> and check cpp reference for std::filesystem functions
+    char* home_dir = getenv("HOME");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
-        DrawText("Test", 0, 0, 24, LIGHTGRAY);
+        ClearBackground(BLANK);
+        DrawFPS(0, 0);
+
+        if (IsKeyDown(KEY_W))
+            DrawText(home_dir, 0, 20, 24, WHITE);
 
         EndDrawing();
     }
