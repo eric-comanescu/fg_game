10a89a0bd89254decc2a66c43c80aa2a7c63310d
Eric Comanescu <ericcom@outlook.com>
2024-10-28 16:24:45 -0400

    Removed #include <iostream> because it caused linking errors (why???)

--- src/Input.cpp
+++ src/Input.cpp
@@ -1,13 +1,13 @@
 #include "../include/Input.h"
 
 #include <vector>
-#include <iostream>
+// #include <iostream>
 
 #include "../include/raylib.h"
 #include "../include/StateMachine.h"
 
 InputManager::InputManager() {
-	std::cout << "Test\n";
+	// std::cout << "Test\n";
 }
 
 InputManager::~InputManager() {
@@ -18,7 +18,7 @@ InputManager::Input InputManager::getInput() {
 	std::vector<KeyboardKey> keysPressed {};
 
 	for (const auto& keybind : m_player1Keybinds) {
-		std::cout << std::get<0>(keybind) << " + " << std::get<1>(keybind) << '\n';
+		// std::cout << std::get<0>(keybind) << " + " << std::get<1>(keybind) << '\n';
 	}
 
 	return {InputManager::MotionInputs::NONE, KEY_W, InputManager::Directions::UpNeutral};
