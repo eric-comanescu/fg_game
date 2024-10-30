5a1d71774ed81a47b723d6f7236b9661d76049b7
Comanescu <2265406@johnabbottcollege.net>
2024-10-30 09:48:55 -0400

    Working on input parser again

--- src/Input.cpp
+++ src/Input.cpp
@@ -4,7 +4,6 @@
 // #include <iostream>
 
 #include "../include/raylib.h"
-#include "../include/StateMachine.h"
 
 InputManager::InputManager() {
 	// std::cout << "Test\n";
@@ -14,12 +13,6 @@ InputManager::~InputManager() {
 
 }
 
-InputManager::Input InputManager::getInput() {
-	std::vector<KeyboardKey> keysPressed {};
-
-	for (const auto& keybind : m_player1Keybinds) {
-		// std::cout << std::get<0>(keybind) << " + " << std::get<1>(keybind) << '\n';
-	}
-
-	return {InputManager::MotionInputs::NONE, KEY_W, InputManager::Directions::UpNeutral};
+void InputManager::updateInput() {
+	if (m_inputQueue.size() == 15) {}
 }
